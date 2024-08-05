eef34e444a0e4fd95b0569f9348616b24bea5933
eliagarc <elias16199@gmail.com>
2024-08-05 10:18:43 +0200

    Eval3

--- Unidad 3/Evaluacion3/src/main/java/com/elias/Main.java
+++ Unidad 3/Evaluacion3/src/main/java/com/elias/Main.java
@@ -2,15 +2,20 @@ package com.elias;
 
 import access.RepositorioIngresos;
 import entities.Ingreso;
+import lombok.AllArgsConstructor;
 import org.h2.tools.Server;
 
+import java.io.File;
+import java.io.ObjectOutputStream;
+import java.net.URI;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 
 public class Main
 {
     public static void main( String[] args ) throws Exception {
-        RepositorioIngresos ingresos = new RepositorioIngresos(Paths.get("./Ingresos.csv"));
+        /*RepositorioIngresos ingresos = new RepositorioIngresos(Paths.get("./Ingresos.csv"));
         ingresos.addTable();
         for (Ingreso ingreso: ingresos.getIngresos()){
             ingresos.add(ingreso);
@@ -21,6 +26,15 @@ public class Main
 
         System.out.println("Press enter to continue...");
         (new java.util.Scanner(System.in)).nextLine();
-        server.stop();
+        server.stop();*/
+        @AllArgsConstructor
+        class Clase1 {
+            private static int dato1;
+            private String dato2;
+        }
+        Clase1 a = new Clase1("2");
+        ObjectOutputStream d = new ObjectOutputStream(a);
+        Path z = new Path("text");
+        File ruta = URI.create("file:///file.text");
     }
 }
